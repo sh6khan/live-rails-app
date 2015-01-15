601cc2cecc18aae2bfc7a2bb22f290763b579544
sh6khan <sadman.khan999@gmail.com>
2015-01-14 22:45:12 -0500

    the app now sends server side events from the index controller

--- app/controllers/browser_controller.rb
+++ app/controllers/browser_controller.rb
@@ -1,5 +1,28 @@
 require 'reloader/sse'
 
 class BrowserController < ApplicationController
-	
+	include ActionController::Live
+
+	def index
+		#SSE expects the 'text/event-stream' content type
+		response.headers['Content-Type'] = 'text/event-stream'
+
+		sse = Reloader::SSE.new(response.stream)
+
+		begin
+			loop do 
+				sse.write({:time => Time.now})
+				sleep 1
+			end  
+		rescue IOError #when the client dissconnect, this will send and IOError with the write
+
+		ensure 
+			sse.close
+		end 
+	end 
+
+	# def index
+	# 	response.headers['Content-Type'] = 'text/event-stream'
+	# 	fail
+	# end 
 end
